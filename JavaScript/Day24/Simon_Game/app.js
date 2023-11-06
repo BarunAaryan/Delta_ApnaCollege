232ec851bf2ee2df30bfd5a2d6167d983ffa187f
Barun Aaryan <barunaryan1@gmail.com>
2023-11-07 00:25:21 +0530

    Started working on Simon Game

---
+++ JavaScript/Day24/Simon_Game/app.js
@@ -0,0 +1,14 @@
+let gameSeq=[];
+let userSeq=[];
+
+let staretd = false;
+let level =0;
+
+document.addEventListener("keypress", function(){
+// console.log("game started")
+//step 1 
+if(started ==false){
+    console.log("Game is started");
+    started = true;
+}
+}); 
