9d1c7dec1dc6e31d7accac7286312a972e90f18c
Barun Aaryan <barunaryan1@gmail.com>
2023-11-22 23:46:44 +0530

    Learnt basics of EJS

--- Backend/Day32/index.js
+++ Backend/Day32/index.js
@@ -1,7 +1,20 @@
 //EJS
 const express = require("express");
 const app = express();
+const path = require("path");
 const port = 3000;
-app.listen(port, ()=>{
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.get("/", (req, res) => {
+    //res.send("This is home");
+    res.render("home.ejs");
+});
+app.get("/hello", (req, res) => {
+    //res.send("This is home");
+    res.send("Hello Guys");
+});
+app.listen(port, () => {
     console.log(`Listening on Port ${port}`);
 })
