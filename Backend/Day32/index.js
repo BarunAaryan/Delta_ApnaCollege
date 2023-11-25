f18b76299d7381a625321e1491c6115c386a6d4d
Barun Aaryan <barunaryan1@gmail.com>
2023-11-25 21:07:10 +0530

    Using of loops in EJS

--- Backend/Day32/index.js
+++ Backend/Day32/index.js
@@ -15,6 +15,22 @@ app.get("/hello", (req, res) => {
     //res.send("This is home");
     res.send("Hello Guys");
 });
+app.get("/rolldice", (req, res ) => {
+    //res.send("This is home");
+    let diceVal= Math.floor(Math.random()*6)+1;
+    res.render("rolldice.ejs", {num: diceVal});
+    // res.render("rolldice.ejs", {diceVal}); when we give same name to the key and the value
+});
 app.listen(port, () => {
     console.log(`Listening on Port ${port}`);
 })
+
+//instagram ejs
+app.get("/ig/:username", (req,res)=>{
+    const followers= ["Adam","Bob", "Steve"];
+let {username}= req.params;
+console.log(username);
+// const instaData = require("./data.json");
+// console.log(instaData);
+res.render("instagram.ejs", {username, followers}); 
+});
