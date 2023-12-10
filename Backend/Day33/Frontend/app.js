7c7a8fd5a61417e41be1c4db4a26543fd5cbb7ce
Barun Aaryan <barunaryan1@gmail.com>
2023-12-10 13:40:14 +0530

    Learned some concepts of OOPS and completed day 33

--- Backend/Day33/Frontend/app.js
+++ Backend/Day33/Frontend/app.js
@@ -24,12 +24,54 @@
 
 //new Operator
 //constructors - does not return anything and start with capital letter
-function Person(name, age){
-       this.name = name;
-       this.age= age;
+// function Person(name, age){
+//        this.name = name;
+//        this.age= age;
+//     }
+//     Person.prototype.talk= function(){
+//         console.log(`Hi, My name is ${this.name}`);
+//     }
+// let p1= new Person("Barun", 21);
+// let p2= new Person("Soumitri", 21);
+
+//creating of constructor
+// class Person{
+//     constructor(name, age){
+//         this.name= name;
+//         this.age= age;
+//     }
+//     talk(){
+//         console.log(`Hi, My name is ${this.name}`);
+//     }
+// }
+// let p1= new Person("Barun", 21);
+// let p2= new Person("Soumitri", 23);
+
+//inheritance
+class Person{
+    constructor(name, age){
+        this.name= name;
+     this.age= age;
     }
-    Person.prototype.talk= function(){
-        console.log(`Hi, My name is ${this.name}`);
+    talk(){
+        console.log(`Hi, I am ${this.name}`);
     }
-let p1= new Person("Barun", 21);
-let p2= new Person("Soumitri", 21);
+}
+class Student extends Person{
+    constructor(name, age, marks){
+        console.log("Student class constructor");
+        super(name, age); //Parent class constructor is being called
+
+        this.marks= marks
+    }
+    
+}
+class Teacher extends Person{
+    constructor(name, age, subject){
+        console.log("Teacher class constructor");
+        super(name, age); //Parent class constructor is being called
+    this.subject = subject;
+    }
+}
+let stu1= new Student("Barun", 21, 35);
+let stu2= new Student("Soumitri", 21, 45);
