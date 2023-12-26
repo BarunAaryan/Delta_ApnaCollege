22244bcf2b6c40a29070158f0c3cefdf94307762
Barun Aaryan <barunaryan1@gmail.com>
2023-12-26 13:31:12 +0530

    Home route- Setting up express

--- Backend/Day37/index.js
+++ Backend/Day37/index.js
@@ -1,5 +1,8 @@
 const { faker } = require('@faker-js/faker');
 const mysql = require('mysql2');
+const express = require("express");
+const app= express();
+
 
 // create the connection to database
 const connection = mysql.createConnection({
@@ -19,19 +22,35 @@ let getRandomUser= ()=>{
     faker.internet.password(),
 ];
 };
-for(let i=1; i<= 100; i++){
-  data.push(getRandomUser()); //100 fake data 
-};
+// for(let i=1; i<= 100; i++){
+//   data.push(getRandomUser()); //100 fake data 
+// };
 
 
-try{
-  connection.query(q, [data], (err, result)=>{
+// try{
+//   connection.query(q, [data], (err, result)=>{
+//     if(err) throw err; 
+//     console.log(result);  
+//   })
+// }catch(err){
+//   console.log(err);
+// } 
+// connection.end(); //to end a connection
+
+app.get("/", (req,res)=>{
+  let q= `SELECT count(*) FROM user`;
+  try{
+  connection.query(q, (err, result)=>{
     if(err) throw err; 
-    console.log(result);  
+    console.log(result[0]["count(*)"]);  
+    res.send("success");
   })
 }catch(err){
   console.log(err);
+  res.send("Some error in database")
 } 
-connection.end(); //to end a connection
+});
 
-
+app.listen("8080", ()=> {
+  console.log("Server is listening to port 8080");
+});
