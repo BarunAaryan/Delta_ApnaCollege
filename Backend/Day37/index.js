3b700b96eed9a8b260af292a338e3ac9f4326002
Barun Aaryan <barunaryan1@gmail.com>
2023-12-29 23:46:00 +0530

    Learnt Edit Route- But have some errors

--- Backend/Day37/index.js
+++ Backend/Day37/index.js
@@ -3,7 +3,10 @@ const mysql = require('mysql2');
 const express = require("express");
 const app= express();
 const path= require("path");
+const methodOverride= require("method-override");
 
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({extended:true})); //to parse the form data
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
@@ -26,7 +29,7 @@ let getRandomUser= ()=>{
 ];
 };
 // for(let i=1; i<= 100; i++){
-//   data.push(getRandomUser()); //100 fake data 
+//   data.push(getRandomUser()); //100 fake data   
 // };
 
 
@@ -68,6 +71,29 @@ try{
   console.log(err);
   res.send("Some error in database");
 } 
+});
+
+//Edit Route
+app.get("/user/:id/edit", (req, res)=>{
+  let {id} = req.params;
+  let q= `SELECT * FROM user WHERE id='${id}'`;
+  res.render("edit.ejs");
+  try{
+    connection.query(q, (err, result)=>{
+      if(err) throw err; 
+      console.log(result);  
+      let user = result;
+      res.render("edit.ejs", { user });
+    });
+  }catch(err){
+    console.log(err);
+    res.send("Some error in database");
+  } 
+});
+
+//UPDATE (db) ROUTE
+app.patch("/user/:id", (req, res)=>{
+res.send("Updated");
 });
 
 app.listen("8080", ()=> {
