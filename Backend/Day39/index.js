6c55e14a84e3c448b8d69b06c833c8d05784e36e
Barun Aaryan <barunaryan1@gmail.com>
2024-01-17 02:34:22 +0530

    InsertOne and InsertMany

--- Backend/Day39/index.js
+++ Backend/Day39/index.js
@@ -19,3 +19,34 @@ const userSchema = new mongoose.Schema({
 }); 
 //models
 const User = mongoose.model("User", userSchema);
+
+//insert in mongoose
+// const user1= new User({
+//     name:"Barun",
+//     email: "barunaryan@gmail.com",
+//     age: 21,
+// });
+//to save the data in mongoose
+// user1.save().then();
+
+// const user2= new User({
+//     name:"Soumitri",
+//     email: "soumitri@gmail.com",
+//     age: 21,
+// });
+// //to save the data in mongoose
+// user2.save().then((res)=>{
+//     console.log(res);
+// }).catch((err)=>{
+//     console.log(err);
+// });
+
+//insert Many
+User.insertMany([
+    {name: "Barkha", email: "barkha@gmail.com", age: 42},
+    {name: "Gudli", email: "gudli@gmail.com", age: 22},
+    {name: "Trisha", email: "trisha@gmail.com", age: 32},
+
+]).then((res)=>{
+    console.log(res);
+});
