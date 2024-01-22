93f3e67ee8dc9391164b32efe206a5fbeaf0b678
Barun Aaryan <barunaryan1@gmail.com>
2024-01-22 16:10:31 +0530

    Creating our Model(chat)

---
+++ Backend/Day40/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app= express();
+const mongoose = require("mongoose");
+const path = require("path");
+const Chat = require("./models/chat.js");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+main()
+.then(()=>{
+    console.log("connection successful");
+})
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+let chat1= new Chat({
+    from: "Barun",
+    to: "Chinki",
+    msg: "I Love You",
+    created_at: new Date()
+});
+chat1.save().then((res)=>{
+    console.log(res);
+})
+
+app.get("/", (req, res)=>{
+res.send("everything is working");
+});
+
+app.listen(8080, ()=>{
+console.log("Server is listening on port 8080");
+});
