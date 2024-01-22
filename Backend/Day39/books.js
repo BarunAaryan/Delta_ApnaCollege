4b426a60d34dba642ddf870b7a5e8c041d470c9b
Barun Aaryan <barunaryan1@gmail.com>
2024-01-22 15:19:21 +0530

    schema validations

--- Backend/Day39/books.js
+++ Backend/Day39/books.js
@@ -22,19 +22,56 @@ author:{
 },
 price:{
     type: Number,
+    min:[0, "price is too low"],
 },
+discount:{
+    type: Number,
+    //Schema type Options
+    default:0,
+},
+category:{
+    type: String,
+    enum:["fiction", "non-fiction"],
+}
 });
 
 
 const Book = mongoose.model("Book", bookSchema);
-let book1 = new Book({
-title: "Half Loved, Half Broken",
-author: "Barun Aaryan",
-price: 200
-});
-book1.save().then(res=>{
-    console.log(res);
-}).catch(err=>{
-console.log(err);
-});
+// let book1 = new Book({
+// title: "Half Loved, Half Broken",
+// author: "Barun Aaryan",
+// price: 200, 
+// });
+// book1.save().then(res=>{
+//     console.log(res);
+// }).catch(err=>{
+// console.log(err);
+// });
+// let book2 = new Book({
+// title: "Gone Girl",
+// price: 399,
+// });
+// book2.save().then(res=>{
+//     console.log(res);
+// }).catch(err=>{
+// console.log(err);
+// });
 
+// let book3 = new Book({
+// title: "Marvel Comics",
+// price: 399,
+// category: "fiction",
+// });
+// book3.save().then(res=>{
+//     console.log(res);
+// }).catch(err=>{
+// console.log(err);
+// });
+
+Book.findByIdAndUpdate("65ae3266894b88f83318d5b1", {price: -500},  {runValidators: true})
+.then((res)=>{
+    console.log(res);
+})
+.catch((err)=>{
+console.log(err.errors.price.properties.message);
+});
