f78126156be49ef1333d0236a4261fbacad03dc7
Barun Aaryan <barunaryan1@gmail.com>
2024-01-24 01:17:42 +0530

    edit route, Update route- getting an error here

--- Backend/Day40/index.js
+++ Backend/Day40/index.js
@@ -3,11 +3,13 @@ const app= express();
 const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
+const methodOverride = require("method-override");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 main()
 .then(()=>{
@@ -63,8 +65,23 @@ app.post("/chats", (req, res)=>{
     res.redirect("/chats");
     });
 
+    //Upadate route
+    app.put("/chats/:id", async (req, res)=>{
+        let {id}= req.params;
+        let {msg: newMsg} = req.body;
+        let updatedChat= await Chat.findByIdAndUpdate(id, {msg: newMsg}, {runValidators: true , new: true} );
+        console.log(updatedChat);
+        res.redirect("/chats");
+    }); 
+
 //New Route
 app.get("/chats/new", (req, res)=>{
 res.render("new.ejs");
-});
+}); 
 
+//Edit route
+app.get("/chats/:id/edit", async (req, res)=>{
+    let {id}= req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs", {chat});
+})
