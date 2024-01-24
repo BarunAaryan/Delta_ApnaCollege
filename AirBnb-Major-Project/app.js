9d23a583e9f8a6ea9cf0d2331d4788a3557416b7
Barun Aaryan <barunaryan1@gmail.com>
2024-01-24 12:14:22 +0530

    Airbnb-Basic Setup

---
+++ AirBnb-Major-Project/app.js
@@ -0,0 +1,21 @@
+const express= require("express");
+const app=  express();
+const mongoose = require("mongoose");
+
+const MONGO_URL= "mongodb://127.0.0.1:27017/wanderlust";
+main().then(()=>{
+    console.log("connected to db");
+}).catch((err)=>{
+    console.log(err)
+});
+
+async function  main(){
+    await mongoose.connect(MONGO_URL);
+}
+
+app.get("/", (req, res)=>{
+res.send("Hi, I am Root");
+});
+app.listen(8080, ()=>{
+    console.log("server is listening to port 8080");
+});
