e46c1e91e660dd86da206cff31675b0669ba3efb
Barun Aaryan <barunaryan1@gmail.com>
2024-01-24 23:13:05 +0530

    read and Create new listings

--- AirBnb-Major-Project/app.js
+++ AirBnb-Major-Project/app.js
@@ -1,30 +1,52 @@
-const express= require("express");
-const app=  express();
+const express = require("express");
+const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 
-const MONGO_URL= "mongodb://127.0.0.1:27017/wanderlust";
-main().then(()=>{
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+main().then(() => {
     console.log("connected to db");
-}).catch((err)=>{
+}).catch((err) => {
     console.log(err)
 });
 
-async function  main(){
+async function main() {
     await mongoose.connect(MONGO_URL);
 }
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.get("/", (req, res)=>{
-res.send("Hi, I am Root");
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.send("Hi, I am Root");
 });
 
 //Index Route
-app.get("/listings", async (req, res)=>{
-    const allListings = await Listing.find({}); 
-    res.render("listings/index.ejs", {allListings});
-    });
+app.get("/listings", async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+});
+//New Route
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs");
+});
+
+//Show route
+app.get("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing });
+});
+
+//Create Route
+app.post("/listings", async (req, res) => {
+    // let listing = req.body.listing;
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+
 
 
 // app.get("/testListing", async (req, res)=>{
@@ -40,6 +62,6 @@ app.get("/listings", async (req, res)=>{
 // console.log("Sample was saved");
 // res.send("Successful testing");
 // });
-app.listen(8080, ()=>{
+app.listen(8080, () => {
     console.log("server is listening to port 8080");
 });
