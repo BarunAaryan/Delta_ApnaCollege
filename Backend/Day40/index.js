75bbd515034307079408e62954d3b74f29d6f164
Barun Aaryan <barunaryan1@gmail.com>
2024-01-24 11:48:19 +0530

    The error got fixed automatically, added destroy route

--- Backend/Day40/index.js
+++ Backend/Day40/index.js
@@ -84,4 +84,12 @@ app.get("/chats/:id/edit", async (req, res)=>{
     let {id}= req.params;
     let chat = await Chat.findById(id);
     res.render("edit.ejs", {chat});
+});
+
+//Destroy route
+app.delete("/chats/:id", async (req, res)=>{
+    let {id}= req.params;
+   let deletedChat = await Chat.findByIdAndDelete(id);
+   console.log(deletedChat);
+   res.redirect("/chats");
 })
