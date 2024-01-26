843f14376ad9faa544e269890df90f65d8f3bb43
Barun Aaryan <barunaryan1@gmail.com>
2024-01-26 18:10:42 +0530

    Styling the index

--- AirBnb-Major-Project/init/data.js
+++ AirBnb-Major-Project/init/data.js
@@ -89,7 +89,7 @@ const sampleListings = [
         "Indulge in luxury living with panoramic city views from this stunning penthouse apartment.",
       image: {
         filename: "Luxury Penthouse with City Views",
-        url: "https://images.unsplash.com/photo-1622396481328-9b1b78cdd9fd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c2t5JTIwdmFjYXRpb258ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60",
+        url: "https://unsplash.com/photos/white-and-gray-couch-on-gray-wooden-floor-yyKsGgQXukY",
       },
       price: 3500,
       location: "Los Angeles",
