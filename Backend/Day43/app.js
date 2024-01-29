a86e8f22e0b68201931cb8070593fe74809dd64b
Barun Aaryan <barunaryan1@gmail.com>
2024-01-29 12:49:52 +0530

    Utility middleware and explored app.use

--- Backend/Day43/app.js
+++ Backend/Day43/app.js
@@ -1,18 +1,37 @@
 const express = require("express");
 const app= express();
 
-app.use((req, res, next)=>{
-console.log("Hi! I am Middleware");
-// res.send("Middleware finished");
-return next(); //if we write like this then the next line is not going to be executed
-console.log("This is after next()");
-//this is genrally not followed but
-});
-app.use((req, res, next)=>{
-console.log("Hi! I am 2nd Middleware");
-// res.send("Middleware finished");
-next();
-});
+// app.use((req, res, next)=>{
+// console.log("Hi! I am Middleware");
+// // res.send("Middleware finished");
+// return next(); //if we write like this then the next line is not going to be executed
+// console.log("This is after next()");
+// //this is genrally not followed but
+// });
+// app.use((req, res, next)=>{
+// console.log("Hi! I am 2nd Middleware");
+// // res.send("Middleware finished");
+// next();
+// });
+
+
+//morgan - logger is popular
+//Creating a utility middleware
+// app.use((req, res, next)=>{
+//     req.time= new Date(Date.now()).toString;
+//     console.log(req.method, req.hostname, req.path, req.time);
+//     next();
+// });
+
+//exploring app.use()
+app.use("/random",(req, res, next )=>{
+    console.log("I am only for random");
+    next();
+})
+//404
+app.use((req, res)=>{
+    res.status(404).send("Page not found")
+})
 
 app.get("/", (req, res)=>{
     res.send("Hi I am groot");
