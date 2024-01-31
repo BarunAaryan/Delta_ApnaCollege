fe6a860358cabb4846656aa274277a4a8486b626
Barun Aaryan <barunaryan1@gmail.com>
2024-01-31 23:20:00 +0530

    Validation for Schema

--- AirBnb-Major-Project/app.js
+++ AirBnb-Major-Project/app.js
@@ -3,10 +3,11 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
-const methodOverride= require("method-override");
+const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const wrapAsync= require("./utils/wrapAsync.js");
-const ExpressError= require("./utils/ExpressError.js");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema } = require("./schema.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 main().then(() => {
@@ -29,6 +30,17 @@ app.get("/", (req, res) => {
     res.send("Hi, I am Root");
 });
 
+//Validate Listing-> Middleware
+const validateListing = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+};
+
 //Index Route
 app.get("/listings", wrapAsync(async (req, res) => {
     const allListings = await Listing.find({});
@@ -47,51 +59,45 @@ app.get("/listings/:id", wrapAsync(async (req, res) => {
 }));
 
 //Create Route
-app.post("/listings", wrapAsync(async (req, res, next) => {
+app.post("/listings", validateListing, wrapAsync(async (req, res, next) => {
     // let listing = req.body.listing;
-    if(!req.body.listing){
-        throw new ExpressError(400, "Send valid data for listing");
-    }
-        const newListing = new Listing(req.body.listing);
-        await newListing.save();
-        res.redirect("/listings");
-    })
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+})
 );
 
 //edit Route
-app.get("/listings/:id/edit", wrapAsync(async (req, res)=>{
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", {listing})
+    res.render("listings/edit.ejs", { listing })
 }));
 
 //Update Route
-app.put("/listings/:id", wrapAsync(async (req, res)=>{
-    if(!req.body.listing){
-        throw new ExpressError(400, "Send valid data for listing");
-    }
+app.put("/listings/:id", validateListing, wrapAsync(async (req, res) => {
     let { id } = req.params;
-    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect(`/listings/${id}`);
 }));
 
 //Delete Route
-app.delete("/listings/:id", wrapAsync(async (req, res)=>{
+app.delete("/listings/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
 }));
 
-app.all("*", (req, res, next)=>{
+app.all("*", (req, res, next) => {
     next(new ExpressError(404, "Page Not Found !"));
 });
 
 //middleware to handle error 
-app.use((err, req, res, next)=>{
-    let{statusCode=500, message= "Something went wrong"} = err;
-    res.status(statusCode).render("error.ejs", {err})
-     // res.status(statusCode).send(message);
+app.use((err, req, res, next) => {
+    let { statusCode = 500, message = "Something went wrong" } = err;
+    res.status(statusCode).render("error.ejs", { err })
+    // res.status(statusCode).send(message);
 });
 
 // app.get("/testListing", async (req, res)=>{
--- AirBnb-Major-Project/models/listing.js
+++ AirBnb-Major-Project/models/listing.js
@@ -10,12 +10,11 @@ const listingSchema = new Schema({
     image: {
         url: {
             type: String,
-            default: "https://unsplash.com/photos/coconut-palm-trees-in-hotel-lobby-_dS27XGgRyQ",
-        set:(v)=> v===""? "" : v,
+            
+            default: "https://images.unsplash.com/photo-1455587734955-081b22074882?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        set:(v)=> v===""? "https://images.unsplash.com/photo-1455587734955-081b22074882?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
         },
         filename: String,
-
-        
     },
     price: Number,
     location: String,
