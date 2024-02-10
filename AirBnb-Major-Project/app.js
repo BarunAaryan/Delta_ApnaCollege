447f3cacbd158d5bd4b0741fb115773c44dc5385
Barun Aaryan <barunaryan1@gmail.com>
2024-02-10 19:32:00 +0530

    Passport and Signup-GET

--- AirBnb-Major-Project/app.js
+++ AirBnb-Major-Project/app.js
@@ -11,9 +11,13 @@ const ExpressError = require("./utils/ExpressError.js");
 // const Review = require("./models/review.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
-const listings = require("./routes/listing.js");
-const reviews= require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 main().then(() => {
@@ -37,8 +41,8 @@ const sessionOptions = {
     resave: false,
     saveUninitialized: true,
     cookie: {
-        expires: Date.now() + 7 *24*60*60*1000, 
-        maxAge: 7 *24*60*60*1000,
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true
     },
 };
@@ -50,16 +54,36 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
-app.use((req, res, next)=>{
-    res.locals.success= req.flash("success");
-    res.locals.error= req.flash("error");
-    console.log( res.locals.succes);
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    console.log(res.locals.succes);
     next();
 });
 
-app.use("/listings", listings);
+app.get("/demouser", async (req, res) => {
+    let fakeUser = new User({
+        email: "student@gmail.com",
+        username: "delta-student"
+    });
+    //to store
+    let registeredUser= await User.register(fakeUser, "helloworld");
+    //these are username and password
+    res.send(registeredUser);
+});
+
+app.use("/listings", listingRouter);
 //reviews
-app.use("/listings/:id/reviews", reviews);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
 
 app.all("*", (req, res, next) => {
