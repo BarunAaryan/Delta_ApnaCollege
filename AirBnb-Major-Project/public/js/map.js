e74a51099d67406b0c53d9d64f5b354f84636373
Barun Aaryan <barunaryan1@gmail.com>
2024-02-22 17:55:02 +0530

    Map Popup

--- AirBnb-Major-Project/public/js/map.js
+++ AirBnb-Major-Project/public/js/map.js
@@ -3,13 +3,16 @@ const map = new mapboxgl.Map({
 container: 'map', // container ID
 // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
 style: 'mapbox://styles/mapbox/satellite-streets-v12', // style URL
-center: coordinates, // starting position [lng, lat]
+center: listing.geometry.coordinates, // starting position [lng, lat]
 zoom: 8 // starting zoom
 });
 
-console.log(coordinates); 
+// console.log(coordinates); 
 
 // Create a default Marker and add it to the map.
 const marker1 = new mapboxgl.Marker({color: 'red'})
-.setLngLat(coordinates) //Listing  geometry coordinates
+.setLngLat(listing.geometry.coordinates) //Listing  geometry coordinates
+.setPopup(new mapboxgl.Popup({offset: 25})
+.setHTML(`<h4>${listing.title}</h4><p>Exact Location provided after booking</p>`
+))
 .addTo(map);
