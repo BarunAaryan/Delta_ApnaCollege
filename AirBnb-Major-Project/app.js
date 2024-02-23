6bfd09257eeadddf5f5320f741845281121fa7d3
Barun Aaryan <barunaryan1@gmail.com>
2024-02-23 20:42:07 +0530

    Making of tax switch and icons

--- AirBnb-Major-Project/app.js
+++ AirBnb-Major-Project/app.js
@@ -53,9 +53,9 @@ const sessionOptions = {
     },
 };
 
-app.get("/", (req, res) => {
-    res.send("Hi, I am Root");
-});
+// app.get("/", (req, res) => {
+//     res.send("Hi, I am Root");
+// });
 
 app.use(session(sessionOptions));
 app.use(flash());
