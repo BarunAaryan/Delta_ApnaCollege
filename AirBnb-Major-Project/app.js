84ff9f1da70dc36392b829f689e1680432c65776
Barun Aaryan <barunaryan1@gmail.com>
2024-02-24 16:28:56 +0530

    Made changes to push it as separate repo

--- AirBnb-Major-Project/app.js
+++ AirBnb-Major-Project/app.js
@@ -49,7 +49,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 const store = MongoStore.create({
     mongoUrl: dbUrl,
     crypto: {
-        secret: "mysupersecretcode"
+        secret: process.env.SECRET,
 },
 touchAfter: 24*3600,
 });
@@ -61,7 +61,7 @@ store.on("error", ()=>{
 
 const sessionOptions = {
     store, //to pass the store info in the express session
-    secret: "mysupersecretcode",
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
