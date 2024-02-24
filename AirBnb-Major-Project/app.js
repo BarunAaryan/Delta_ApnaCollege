9830ef6437584c3636d402cc8904d6e81a1614e2
Barun Aaryan <barunaryan1@gmail.com>
2024-02-24 13:44:53 +0530

    Using mongo atlas and mongo session store- Putting the db online

--- AirBnb-Major-Project/app.js
+++ AirBnb-Major-Project/app.js
@@ -1,4 +1,4 @@
-if(process.env.NODE_ENV != "production"){
+if (process.env.NODE_ENV != "production") {
     require("dotenv").config();
 }
 
@@ -16,6 +16,7 @@ const ExpressError = require("./utils/ExpressError.js");
 // const { listingSchema, reviewSchema} = require("./schema.js");
 // const Review = require("./models/review.js");
 const session = require("express-session");
+const MongoStore = require('connect-mongo');
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -25,7 +26,9 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ATLASDB_URL;
+
 main().then(() => {
     console.log("connected to db");
 }).catch((err) => {
@@ -33,7 +36,8 @@ main().then(() => {
 });
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+    // await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbUrl);
 }
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -42,7 +46,21 @@ app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto: {
+        secret: "mysupersecretcode"
+},
+touchAfter: 24*3600,
+});
+
+//to check for errors
+store.on("error", ()=>{
+    console.log("error in MONGO SESSION STORE" , err);
+});
+
 const sessionOptions = {
+    store, //to pass the store info in the express session
     secret: "mysupersecretcode",
     resave: false,
     saveUninitialized: true,
@@ -56,6 +74,8 @@ const sessionOptions = {
 // app.get("/", (req, res) => {
 //     res.send("Hi, I am Root");
 // });
+
+
 
 app.use(session(sessionOptions));
 app.use(flash());
