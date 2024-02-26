bcf726dc39d20b2c5313634be7d571420fc6e7b3
Barun Aaryan <barunaryan1@gmail.com>
2024-02-26 19:59:51 +0530

    import-export

--- React/Day56_React(part1)/basic-react-app/src/App.jsx
+++ React/Day56_React(part1)/basic-react-app/src/App.jsx
@@ -1,7 +1,21 @@
-import "./App.css"
+import "./App.css";
+import Title from "./Title.jsx";
+//import
+
+function Description() {
+  return <h3>I am the description</h3>;
+}  
 
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <div>
+      <h1>This is my app component</h1>
+      <p> Inside app component we have: </p>
+      <Title />
+      <Title />
+      <Description/>
+    </div>
+  );
 }
 
-export default App
+export default App; 
---
+++ React/Day56_React(part1)/basic-react-app/src/Title.jsx
@@ -0,0 +1,7 @@
+function Title() {
+    return <h1>I am the title</h1>;
+  }
+
+
+// export
+export default Title;
