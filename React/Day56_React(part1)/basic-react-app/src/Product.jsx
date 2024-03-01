b6205abf830fdfded9c101132ec923c9931390df
Barun Aaryan <barunaryan1@gmail.com>
2024-03-01 23:23:49 +0530

    Dynamic component styling

--- React/Day56_React(part1)/basic-react-app/src/Product.jsx
+++ React/Day56_React(part1)/basic-react-app/src/Product.jsx
@@ -3,12 +3,14 @@ import "./Product.css"
 function Product({title, price, features}){
     // const list= features.map((feature)=> <li>{feature}</li>)
 // console.log(features);
+let isDiscount = price > 30000;
+let styles= {backgroundColor: isDiscount ? "yellow": ""};
     return (
-        <div className="Product">
+        <div className="Product" style={styles}>
             <h3>{title}</h3>
             <h5> Price: {price}</h5>
             {price> 30000 ? <p>"5% Discount"</p> :<a href="/">Get Discount</a>}
-        </div>
+        </div> 
     ); 
 }
 export default Product;
