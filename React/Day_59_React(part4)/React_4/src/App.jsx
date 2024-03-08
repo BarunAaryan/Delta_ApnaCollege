766cd2e0b1b6cbfc0032afd0d7a7ef7faa4991a0
Barun Aaryan <barunaryan1@gmail.com>
2024-03-08 20:33:56 +0530

    Todo List App

--- React/Day_59_React(part4)/React_4/src/App.jsx
+++ React/Day_59_React(part4)/React_4/src/App.jsx
@@ -1,15 +1,14 @@
 import './App.css';
-import LudoBoard from "./LudoBoard";
+// import LudoBoard from "./LudoBoard";
+import TodoList from './TodoList';
 
 function App() {
 
-
   return (
     <>
-      <LudoBoard/>
-      
+      <TodoList/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ React/Day_59_React(part4)/React_4/src/TodoList.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export default function TodoList() {
+
+    let [todos,setTodos] = useState(["wow"]);
+    let [newTodo, setNewTodo] = useState("");
+
+    let addNewTask = () => {
+        setTodos([...todos,newTodo]);
+        setNewTodo("");
+    };
+
+    let updateTodoValue = (event)=>{
+        setNewTodo(event.target.value);
+    };
+
+    return (
+        <div>
+        <input 
+        placeholder="add a task" 
+        value={newTodo} 
+        onChange={updateTodoValue}>
+        </input>
+        <button onClick={addNewTask} >add task</button>
+        <br /><br /><br /><br /><hr />
+        <h4> Task Todo </h4>
+        <ul>
+            {todos.map((todo) => (
+                <li>{todo}</li>
+            ))}
+               
+     </ul>
+        </div>
+    );
+}
