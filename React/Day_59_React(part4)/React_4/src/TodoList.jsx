854f0ab9dd9742624e7ba6221b1367fb10bed7d3
Barun Aaryan <barunaryan1@gmail.com>
2024-03-16 00:18:15 +0530

    Deleting From Arrays

--- React/Day_59_React(part4)/React_4/src/TodoList.jsx
+++ React/Day_59_React(part4)/React_4/src/TodoList.jsx
@@ -3,34 +3,44 @@ import { v4 as uuidv4 } from 'uuid';
 
 export default function TodoList() {
 
-    let [todos,setTodos] = useState([{task:"sample-task", id:uuidv4()}]);
+    let [todos, setTodos] = useState([{ task: "sample-task", id: uuidv4() }]);
     let [newTodo, setNewTodo] = useState("");
 
     let addNewTask = () => {
-        setTodos([...todos,{task: newTodo, id: uuidv4()}]);
+        setTodos((prevTodos) => {
+            return [...prevTodos, { task: newTodo, id: uuidv4() }]
+        });
         setNewTodo("");
     };
 
-    let updateTodoValue = (event)=>{
+    let updateTodoValue = (event) => {
         setNewTodo(event.target.value);
+    };
+    let deleteTodo = (id) => {
+        setTodos((prevTodos)=> todos.filter((prevTodos)=> prevTodos.id !=id));
+        
     };
 
     return (
         <div>
-        <input 
-        placeholder="add a task" 
-        value={newTodo} 
-        onChange={updateTodoValue}>
-        </input>
-        <button onClick={addNewTask} >add task</button>
-        <br /><br /><br /><br /><hr />
-        <h4> Task Todo </h4>
-        <ul>
-            {todos.map((todo) => (
-                <li key={todo.id}>{todo.task}</li>
-            ))}
-    
-     </ul>
+            <input
+                placeholder="add a task"
+                value={newTodo}
+                onChange={updateTodoValue}>
+            </input>
+            <button onClick={addNewTask} >add task</button>
+            <br /><br /><br /><br /><hr />
+            <h4> Task Todo </h4>
+            <ul>
+                {todos.map((todo) => (
+                    <li key={todo.id}>
+                        <span>{todo.task}</span>
+                        &nbsp;&nbsp;&nbsp;
+                        <button onClick={()=>deleteTodo(todo.id)} >Delete</button>
+                    </li>
+                ))}
+
+            </ul>
         </div>
     );
 }
